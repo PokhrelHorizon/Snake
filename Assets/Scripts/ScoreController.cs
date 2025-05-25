13ad4ec4f38a9b459845b9ba14bd9048500d0b47
PokhrelHorizon (Kshitiz) <131946720+PokhrelHorizon@users.noreply.github.com>
2025-05-25 15:56:01 +0545

    Removed feature
    Removed best board feature as it doesn't make sense to keep same best score across different level types.

--- Assets/Scripts/ScoreController.cs
+++ Assets/Scripts/ScoreController.cs
@@ -2,30 +2,13 @@ using UnityEngine;
 using TMPro;
 public class ScoreController : MonoBehaviour
 {
-    [SerializeField] TMP_Text scoreTMP, bestTMP;
+    [SerializeField] TMP_Text scoreTMP;
 
     private int score;
-    //initialize best if never played before
-    private void Start()
-    {
-        if(!PlayerPrefs.HasKey("bestscore"))
-        {
-            PlayerPrefs.SetInt("bestscore", 0);
-            PlayerPrefs.Save();
-        }
-        bestTMP.text = PlayerPrefs.GetInt("bestscore").ToString(); //shows best score from game start
-    }
 
     public void UpdateScore()
     {
         score++;
-        if(PlayerPrefs.GetInt("bestscore") < score)
-        {
-            PlayerPrefs.SetInt("bestscore", score);
-            PlayerPrefs.Save();
-        }
-
         scoreTMP.text = score.ToString();
-        bestTMP.text = PlayerPrefs.GetInt("bestscore").ToString();
     }
 }
